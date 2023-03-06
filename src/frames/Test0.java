52dbaa426c6f6b7dac015231ada06901dafbea5a
kukon miah <kukon11674@gmail.com>
2023-03-05 20:41:49 -0500

    updated SeleniumAutomation

--- src/frames/Test0.java
+++ src/frames/Test0.java
@@ -2,7 +2,9 @@ package frames;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Test0 {
 
@@ -12,16 +14,23 @@ public class Test0 {
 	    driver.get("https://www.snapdeal.com/");
 	    //driver.manage().window().maximize();
 	    
-	    driver.findElement(By.xpath("//span[text()='Sign In']")).click();
+	    WebElement ele = driver.findElement(By.xpath("//span[text()='Sign In']"));
+	    Actions a=new Actions(driver);
+	    a.moveToElement(ele).perform();
+	    
 	    driver.findElement(By.xpath("//a[text()='login']")).click();
 	    driver.switchTo().frame(0);
 	    Thread.sleep(5000);
-	    driver.findElement(By.xpath("//span[text()='Google']")).click();
 	    
+	    driver.findElement(By.id("close-pop")).click();
+	    driver.switchTo().defaultContent();
+	    Thread.sleep(5000);
+	    
+	    driver.findElement(By.id("inputValEnter")).sendKeys("phone");
+	    Thread.sleep(5000);
+	    driver.close();
+	    //this is frames
 	   
-	    
-	    
-
 	}
 
 }
--- src/testNG/Tc2.java
+++ src/testNG/Tc2.java
@@ -1,5 +1,6 @@
 package testNG;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,6 +8,7 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class Tc2 {
 	public WebDriver driver;
@@ -20,9 +22,14 @@ public class Tc2 {
 	public void tc2() {
 		driver.findElement(By.linkText("Create a Page")).click();
 		String title = driver.getTitle();
+		//Assert.assertEquals(title, "678903334");
+		//Assert.assertEquals(title, "Facebook");
+		SoftAssert s=new SoftAssert();
+		s.assertEquals(title, "kukonmiah");
 		Reporter.log(title,true);
 		String url = driver.getCurrentUrl();
 		Reporter.log(url,true);
+		s.assertAll();
 	}
 	@AfterMethod
 	public void closapp() {
